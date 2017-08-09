f54867e711921eae7047e03a92c6997d67963ad0
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2017-08-09 16:26:13 +0300

    Use Hedgehog 0.5.

--- Haskell/MaitreDTests.hs
+++ Haskell/MaitreDTests.hs
@@ -3,25 +3,26 @@ module MaitreDTests
     , tryAcceptBehavesCorrectlyWhenItCanNotAccept
     ) where
 
-import           Control.Monad      (liftM2)
-import           Data.Maybe         (isNothing)
-import           Data.Time          (LocalTime (..), ZonedTime (..), midnight,
-                                     utc)
-import           Data.Time.Calendar (fromGregorian, gregorianMonthLength)
+import           Control.Monad         (liftM2)
+import           Data.Functor.Identity (Identity (..))
+import           Data.Maybe            (isNothing)
+import           Data.Time             (LocalTime (..), ZonedTime (..),
+                                        midnight, utc)
+import           Data.Time.Calendar    (fromGregorian, gregorianMonthLength)
 import           MaitreD
 
 import           Hedgehog
-import qualified Hedgehog.Gen       as Gen
-import qualified Hedgehog.Range     as Range
+import qualified Hedgehog.Gen          as Gen
+import qualified Hedgehog.Range        as Range
 
-genZonedTime :: Monad m => Gen m ZonedTime
+genZonedTime :: GenT Identity ZonedTime
 genZonedTime = do
     y <- Gen.int (Range.constant 1 9999)
     m <- Gen.int (Range.constant 1 12)
     d <- Gen.int (Range.constant 1 (gregorianMonthLength (toInteger y) m))
     return $ ZonedTime (LocalTime (fromGregorian (toInteger y) m d) midnight) utc
 
-genReservation :: Monad m => Gen m Reservation
+genReservation :: GenT Identity Reservation
 genReservation = do
   bookingDate <- genZonedTime
   positiveQty <- Gen.int (Range.linear 1 100)
